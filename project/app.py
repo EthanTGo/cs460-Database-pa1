28a75ecfe11f42a3b57a47bc33b38f3abcc7d17c
EthanTGo <ethango@bu.edu>
2019-11-03 20:05:48 -0500

    fixed bug

--- project/app.py
+++ project/app.py
@@ -15,55 +15,71 @@ def homepage():
         print(task_content)
         try:
             result = retrieve_info(task_content)
-            return render_template('pa1_website_QID.html',header = 'Executed Successfully',  result=result)
+            return render_template('pa1_website_QID.html', header='Executed Successfully', result=result)
         except:
-            return render_template('pa1_website_QID.html', header = 'Something went wrong', result='')
+            return render_template('pa1_website_QID.html', header='Something went wrong', result='')
     else:
-        return render_template('pa1_website_QID.html', header = 'Welcome enter your query', result='')
+        return render_template('pa1_website_QID.html', header='Welcome enter your query', result='')
 
-@app.route('/checks_in',  methods=['POST', 'GET'])
+
+@app.route('/checks_in', methods=['POST', 'GET'])
 def check_in():
     if request.method == 'POST':
         user_id = request.form['checksinUser_id']
         business_id = request.form['checksinBusiness_id']
         day_of_week = request.form['options']
         date = request.form['checksinDate']
-        to_add = "insert into checks_in (business_id, user_id, check_in_date) values (" + business_id + ", " + user_id + ", DEFAULT)"
+        to_add = "insert into checks_in (business_id, user_id, check_in_date) values ('" + business_id + "', '" + user_id + "', '" + date + "')"
         print(to_add)
         if day_of_week != '':
             try:
                 add_query(to_add)
-                result = retrieve_info('select * from checks_in where user_id =' + user_id)
-                return render_template('pa1_website_userCheckin.html', header='Executed successfully',result = result)
+                result = retrieve_info("select * from checks_in where user_id = '" + user_id + "'")
+                # now update day
+                day_update = "update checkins set " + day_of_week + " = " + day_of_week + " + 1 where business_id = '" + business_id + "'"
+                add_query(day_update)
+                return render_template('pa1_website_userCheckin.html', header='Executed successfully', result=result)
             except:
-                return render_template('pa1_website_userCheckin.html', header = 'Something went wrong', result= '')
+                return render_template('pa1_website_userCheckin.html', header='Something went wrong', result='')
         else:
-            return render_template('pa1_website_userCheckin.html' , header = 'You must choose a day', result='')
+            return render_template('pa1_website_userCheckin.html', header='You must choose a day', result='')
     else:
-        return render_template('pa1_website_userCheckin.html', header='Execute something to see what happens', result='')
+        return render_template('pa1_website_userCheckin.html', header='Execute something to see what happens',
+                               result='')
 
-@app.route('/write_review', methods=['POST', 'GET'])
+
+@app.route('/write_reviews', methods=['POST', 'GET'])
 def write_review():
     if request.method == 'POST':
         user_id = request.form['reviewUser_id']
+        print(type(user_id))
         business_id = request.form['reviewBusiness_id']
+        print(type(business_id))
         date = request.form['checksinDate']
+        print(type(date))
         review_text = request.form['text']
-        stars = int(request.form['options'])
+        print(type(review_text))
+        stars = request.form['options']
+        print(type(str(stars)))
+        tuple1 = retrieve_info('select * from reviews')
+        print(tuple1)
+        num = str(len(retrieve_info('select * from reviews')))
+        print(type(num))
+        print('' + user_id + business_id + date + review_text + stars)
+        to_add = "insert into reviews (review_id, business_id, user_id, stars, review_date, votes_funny, votes_useful, votes_cool, review_text) values (" + num + ",'" + business_id + "','" + user_id + "'," + stars + ",'" + date + "', 0, 0, 0, '" + review_text + "')"
+        print(to_add)
         if stars != '':
             try:
-                num = hash(len(retrieve_info('select count * from reviews')))
-                to_add = "insert into reviews (review_id, business_id, user_id, stars, review_data, votes_funny, votes_useful, votes_cool) values ('" + num + "','" + business_id + "','" + user_id + "'," + stars + ",'" + date + "', 0, 0, 0, '" + review_text + "')"
                 add_query(to_add)
                 result = retrieve_info('select * from reviews')
-                return render_template('pa1_website_writeReview.html', header = 'Executed successfully', result = result)
+                add_query("update users set review_count = review_count + 1 where user_id = '" + user_id  +"'")
+                return render_template('pa1_website_writeReview.html', header='Executed successfully', result=result)
             except:
                 return render_template('pa1_website_writeReview.html', header='Something went wrong', result='')
         else:
             return render_template('pa1_website_writeReview.html', header=' Please choose a star', result='')
-
     else:
-        return render_template('pa1_website_writeReview.html', header ='Execute something to start', result='')
+        return render_template('pa1_website_writeReview.html', header='Execute something to start', result='')
 
 
 app.run(port=5000)
